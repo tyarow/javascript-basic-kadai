91199fb8e84c22b92a60f508a5dc8ca89fa3dba1
tyarow <tyarow01@me.com>
2025-06-23 22:25:21 +0900

    kadai_007をアップロードしました

---
+++ kadai_007/calc.js
@@ -0,0 +1,16 @@
+// numの定義
+let num = 2;
+
+// 条件分岐
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log('3と5の倍数です');
+} 
+else if (num % 5 === 0) {
+  console.log('5の倍数です')
+}
+else if (num % 3 === 0) {
+  console.log('3の倍数です')
+}
+else {
+  console.log(num);
+}
